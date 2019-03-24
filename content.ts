64b0a940af337f3ea2ffda547450026e3a450a72
Robert Maxton <robert.maxton42@gmail.com>
2019-03-23 22:58:22 -0700

    declevered; wrote watcher spawning logic

--- content.ts
+++ content.ts
@@ -26,59 +26,38 @@ class ClearAllEvent extends CustomEvent<any> {
     }
 }
 
-class contentWatcher {
+class ContentWatcher {
     count: number
     cacheSize: number
-    clearall: (ev: CacheFillEvent) => void
+    state: string
 
     constructor(e: HTMLElement, cacheSize: number) {
         //Fire a CacheFillEvent after cacheCount input events (new characters).
         //Round up to the nearest character if using an IME (compositionend).
         this.count = 0;
         this.cacheSize = cacheSize;
+        this.state = Utility.agnosticGetInnerHTML(e);
 
-        this.clearall = function(ev: CacheFillEvent) {
-            let diff = differ.diff_main(ev.detail.original, ev.detail.delta);
-            differ.diff_cleanupEfficiency(diff);
-            let patchfold = diff.reduce<T.PatchFold>((acc:T.PatchFold, nxt) => {
-                const patchl = nxt[1].length;
-                switch(nxt[0]) {
-                    case -1: 
-                         return {
-                             cursor: acc.cursor, //deletion doesn't move the cursor
-                             patchlist: acc.patchlist.concat([[T.PatchType.Deletion, acc.cursor, patchl]])
-                         }
-                    case 0:
-                         return {
-                             cursor: acc.cursor + patchl,
-                             patchlist: acc.patchlist
-                         }
-                    case 1:
-                         return {
-                             cursor: acc.cursor + patchl,
-                             patchlist: acc.patchlist.concat([[T.PatchType.Insertion, acc.cursor, nxt[1]]])
-                         }
-                }
-            }, { cursor: 0, patchlist: [] });
-            submitDraft(patchfold.patchlist);
-        }
         e.addEventListener('input', this.track);
-        e.addEventListener('cachefill', )
+        e.addEventListener('cachefill', this.cacheFill)
 
         //Fire early if the user does something that looks like a transposition
-        e.addEventListener('cut', (ev) => track(ev, true));
+        e.addEventListener('cut', (ev) => this.track(ev, true));
         e.addEventListener('copy', (ev) => {
             let psuedocut = function (ev: KeyboardEvent){
                 if ( ["Backspace", "Clear", "Delete"].some(v => (v == ev.key)) )
-                    track(ev, true);
+                    this.track(ev, true);
                 ev.target.removeEventListener('keydown', psuedocut);
                 }
             ev.target.addEventListener('keydown', psuedocut);
         })
-        e.addEventListener('dragstart', (ev) => track(ev, true));
+        e.addEventListener('dragstart', (ev) => this.track(ev, true));
         //TODO Make the background handler check for a deletion
         //followed by an insertion of the same characters.
+
+
     }
+
     track(ev: Event, override: boolean = false) {
         this.count++;
         if (override || this.count >= this.cacheSize) {
@@ -86,11 +65,11 @@ class contentWatcher {
             function fire(){
                 const delta = Utility.agnosticGetInnerHTML(<HTMLElement>ev.target);
                 const details = { 
-                    original: original,
+                    original: this.state,
                     delta: delta
                 }
                 ev.target.dispatchEvent(new CacheFillEvent(details)); 
-                original = delta;
+                this.state = delta;
             }
             if(ev instanceof InputEvent && ev.isComposing) {
                 //Wait for compositionend
@@ -100,6 +79,36 @@ class contentWatcher {
             }
         }
     }
+
+    cacheFill(ev: CacheFillEvent) {
+        let diff = differ.diff_main(ev.detail.original, ev.detail.delta);
+        differ.diff_cleanupEfficiency(diff);
+        let patchfold = diff.reduce<T.PatchFold>((acc:T.PatchFold, nxt) => {
+            const patchl = nxt[1].length;
+            switch(nxt[0]) {
+                case -1: 
+                     return {
+                         cursor: acc.cursor, //deletion doesn't move the cursor
+                         patchlist: acc.patchlist.concat([[T.PatchType.Deletion, acc.cursor, patchl]])
+                     }
+                case 0:
+                     return {
+                         cursor: acc.cursor + patchl,
+                         patchlist: acc.patchlist
+                     }
+                case 1:
+                     return {
+                         cursor: acc.cursor + patchl,
+                         patchlist: acc.patchlist.concat([[T.PatchType.Insertion, acc.cursor, nxt[1]]])
+                     }
+            }
+        }, { cursor: 0, patchlist: [] });
+        this.submitDraft(patchfold.patchlist);
+    }
+
+    async submitDraft(draft: Array<T.Patch>) : Promise<number> {
+        return browser.runtime.sendMessage(draft);
+    }
 }
 
 
@@ -108,14 +117,17 @@ class contentWatcher {
 //including e.g. Xenforo's own draft functionality.
 
 let mincount: Promise<number> = browser.storage.sync.get('mincount');
+let cachesize: Promise<number> = browser.storage.sync.get('cachesize');
+let watchers = [];
+let differ = new diff_match_patch();
 let fresh = true;
-const differ = new diff_match_patch()
 
-function countWatcher(e: InputEvent) {
-    const count = (<HTMLElement>e.target).innerHTML.length;
-
+async function watcherSpawner(e: InputEvent) {
+    const target = <HTMLElement> e.target;
+    const count = target.innerHTML.length;
+    if (count > await mincount) {
+        watchers.push(new ContentWatcher(target, await cachesize));
+    }
 }
 
-async function submitDraft(draft: Array<T.Patch>) : Promise<number> {
-    return browser.runtime.sendMessage(draft);
-}
+window.addEventListener("input", watcherSpawner);
