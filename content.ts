deec9edee13ee64017f5cb45cc43d5b45fea6142
Robert Maxton <robert.maxton42@gmail.com>
2019-03-24 16:04:07 -0700

    clearall logic; leaving deconstruct to later beceause it needs to be thorough

--- content.ts
+++ content.ts
@@ -14,14 +14,13 @@ class CacheFillEvent extends CustomEvent<T.CachedDelta> {
     }
 }
 
-class ClearAllEvent extends CustomEvent<any> {
+class ClearAllEvent extends CustomEvent<never> {
     target: HTMLElement;
 
-    constructor(details) {
+    constructor() {
         super('clearall', {
             bubbles: true,
-            cancelable: false,
-            detail: details
+            cancelable: false
         });
     }
 }
@@ -30,16 +29,18 @@ class ContentWatcher {
     count: number
     cacheSize: number
     state: string
+    slot: number
 
-    constructor(e: HTMLElement, cacheSize: number) {
+    constructor(e: HTMLElement, cacheSize: number, slot: number) {
         //Fire a CacheFillEvent after cacheCount input events (new characters).
         //Round up to the nearest character if using an IME (compositionend).
         this.count = 0;
         this.cacheSize = cacheSize;
         this.state = Utility.agnosticGetInnerHTML(e);
+        this.slot = slot;
 
         e.addEventListener('input', this.track);
-        e.addEventListener('cachefill', this.cacheFill)
+        e.addEventListener('cachefill', this.handleCacheFill)
 
         //Fire early if the user does something that looks like a transposition
         e.addEventListener('cut', (ev) => this.track(ev, true));
@@ -55,7 +56,8 @@ class ContentWatcher {
         //TODO Make the background handler check for a deletion
         //followed by an insertion of the same characters.
 
-
+        e.addEventListener('input', this.defineClearAll);
+        e.addEventListener('clearall', this.handleClearAll);
     }
 
     track(ev: Event, override: boolean = false) {
@@ -80,7 +82,23 @@ class ContentWatcher {
         }
     }
 
-    cacheFill(ev: CacheFillEvent) {
+    /**
+     * A ClearAllEvent is defined as any Input event
+     * where the target starts with >1 characters and ends with 
+     * 0 or 1. 
+     * 
+     * Technically that means there ought to be a check
+     * for >1 characters, but since ContentWatchers aren't
+     * aassigned until the character count passes
+     * a certain size it should basically never come up.
+     */
+    defineClearAll(ev: InputEvent) {
+        if((<HTMLElement>ev.target).innerHTML.length < 2) {
+            ev.target.dispatchEvent(new ClearAllEvent());
+        }
+    }
+
+    handleCacheFill(ev: CacheFillEvent) {
         let diff = differ.diff_main(ev.detail.original, ev.detail.delta);
         differ.diff_cleanupEfficiency(diff);
         let patchfold = diff.reduce<T.PatchFold>((acc:T.PatchFold, nxt) => {
@@ -106,6 +124,10 @@ class ContentWatcher {
         this.submitDraft(patchfold.patchlist);
     }
 
+    handleClearAll(ev: ClearAllEvent) {
+        this.deconstruct();   
+    }
+
     async submitDraft(draft: Array<T.Patch>) : Promise<number> {
         return browser.runtime.sendMessage(draft);
     }
@@ -126,6 +148,7 @@ async function watcherSpawner(e: InputEvent) {
     const target = <HTMLElement> e.target;
     const count = target.innerHTML.length;
     if (count > await mincount) {
+        //TODO Assign new slot number
         watchers.push(new ContentWatcher(target, await cachesize));
     }
 }
